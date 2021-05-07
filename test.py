540bba9bfc15500a31826ba831536778f0493769
임창환 <solo_monster@naver.com>
2021-05-07 16:00:06 +0900

    make png file for all tickers(coins) and show all graphs

--- test.py
+++ test.py
@@ -3,15 +3,22 @@ import matplotlib.pyplot as plt
 import cv2
 
 # get pandas dataframe and make matplot .png file
-df = pyupbit.get_ohlcv('KRW-BTC',interval="minute90")['close']
-plt.plot(df)
-plt.savefig('temp.png')
+tickers = pyupbit.get_tickers("KRW")
+for i in tickers:
+    df = pyupbit.get_ohlcv(i,interval="minute30")
+    df = df['close']
+    plt.plot(df)
+    plt.savefig(i)
+    plt.clf()
 
 # from .png file to opencv for input file
 x= 100;y=70;w=455;h=345
-graph = cv2.imread('temp.png')[y:y+h,x:x+w].copy()
+for i in tickers:
+    graph = cv2.imread(i+".png")[y:y+h,x:x+w].copy()
+    
+    # just show result img
+    cv2.imshow("result",graph)
+    key = cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# just show result img
-cv2.imshow("result",graph)
-key = cv2.waitKey(0)
-cv2.destroyAllWindows()
+
