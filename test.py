ac5b953a2b70ac6368c474ea28c682c49261d03a
임창환 <solo_monster@naver.com>
2021-05-07 15:31:43 +0900

    make code simple and add comments

--- test.py
+++ test.py
@@ -2,17 +2,16 @@ import pyupbit
 import matplotlib.pyplot as plt
 import cv2
 
-df = pyupbit.get_ohlcv('KRW-BTC',interval="minute30")['close']
+# get pandas dataframe and make matplot .png file
+df = pyupbit.get_ohlcv('KRW-BTC',interval="minute90")['close']
 plt.plot(df)
 plt.savefig('temp.png')
 
-img = cv2.imread('temp.png')
+# from .png file to opencv for input file
 x= 100;y=70;w=455;h=345
-roi = img[y:y+h,x:x+w]
-img2 = roi.copy()
+graph = cv2.imread('temp.png')[y:y+h,x:x+w].copy()
 
-cv2.imshow("roi",img2)
-
+# just show result img
+cv2.imshow("result",graph)
 key = cv2.waitKey(0)
-print(key)
 cv2.destroyAllWindows()
