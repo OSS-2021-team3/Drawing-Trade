96ee9ee4cc3ccc1d32b647633e56197340199a41
임창환 <solo_monster@naver.com>
2021-05-07 15:18:42 +0900

    png to opencv only graph roi with show

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
 import pyupbit
 import matplotlib.pyplot as plt
+import cv2
 
-df = pyupbit.get_ohlcv('KRW-BTC',interval="minute30")
-print(df)
+df = pyupbit.get_ohlcv('KRW-BTC',interval="minute30")['close']
+plt.plot(df)
+plt.savefig('temp.png')
 
-plt.plot(df['close'])
-plt.savefig('foo.png')
+img = cv2.imread('temp.png')
+x= 100;y=70;w=455;h=345
+roi = img[y:y+h,x:x+w]
+img2 = roi.copy()
+
+cv2.imshow("roi",img2)
+
+key = cv2.waitKey(0)
+print(key)
+cv2.destroyAllWindows()
