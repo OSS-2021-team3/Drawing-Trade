aae7a123ca60d46b0ceb401d16d64b16f950756c
Ku <rnxotjd123@gmail.com>
2021-05-11 21:03:44 +0900

    Improve matching algorithm
    -매칭 알고리즘 테스트 , 개선
    -Humoment Matching은 너무 부정확해서 삭제

--- AverageHashMatching.py
+++ AverageHashMatching.py
@@ -1,10 +1,11 @@
 import cv2
-import numpy as np
 
 # 이미지를 32x32크기의 평균 해쉬로 변환
 def img2hash(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (32, 32))
+    thresh = 127
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, (256, 256))
+    ret,gray = cv2.threshold(gray,thresh,255,cv2.THRESH_BINARY)
     avg = gray.mean()
     bi = 1 * (gray > avg)
     return bi
@@ -18,10 +19,10 @@ def hamming_distance(a, b):
     return distance
 
 # 이미지 HashMathcing, 0~1사이의 float값 리턴
-# 1에 가까울 수록 잘맞음
 # 이미지를 binary형태로 만들고 hamming distance 값 구하는거 
+# 0에 가까울 수록 (값이 작을 수록) 비슷한거임
 def AverageHashMatching(img1,img2):
     img1_hash = img2hash(img1)
     img2_hash = img2hash(img2)
-    dst = hamming_distance(img1_hash,img2_hash)/1024
+    dst = hamming_distance(img1_hash,img2_hash)/(256*256)
     return dst
