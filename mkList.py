07e37042938a6b775a5f5de6119a8d5f200fd009
Bang SungWon <bsw9698@gmail.com>
2021-06-03 20:59:12 +0900

    update gui.py & ui

---
+++ mkList.py
@@ -0,0 +1,42 @@
+import crawler
+import numpy as np
+import cv2
+
+# import algorithms
+from ImageMatching import ImageMatching
+
+# make [ [name,diff] , [name,diff]  ... ]
+def mkList(user_img_url,case=1,data_path = "./datas/"):
+    print(user_img_url)
+    user_img = cv2.imread(user_img_url)
+    #  update price datas
+    tickers = crawler.carwl(data_path)
+    # tickers = crawler.gettickers()
+
+    # maybe add loading page
+    # choose one algorithm
+    if case == 1:
+        compare = ImageMatching.EuclideanDistanceMatching
+        print("Euclide matching start")
+    elif case == 2:
+        print("Cosine matching start")
+        compare = ImageMatching.CosineSimilarityMatching
+
+    values = []
+    # valid data space
+    x= 100;y=70;w=455;h=345
+
+    # compare each graph with a choosen algorithm 
+    for i in tickers:
+        # from .png file to opencv for input file
+        graph = cv2.imread(data_path+i+".png")[y:y+h,x:x+w].copy()
+
+        # make 2nd dimension array
+        temp = [i]
+        temp.append(compare(user_img,graph))
+        values.append(temp)
+    
+    # sort least diffrence
+    values.sort(key = lambda x:x[1])
+
+    return values
